557891fbc6328e7edde3c664df83514c7cdfc7c0
Eugene Kiprop Sang <85967099+eugenesang@users.noreply.github.com>
2021-09-05 11:59:09 +0300

    Create WhatsAppChatToJSON.js

---
+++ WhatsAppChatToJSON.js
@@ -0,0 +1,21 @@
+function whatsappToJSON(whatsappChat){
+    ntext=whatsappChat.split(`
+`);
+let arr=new Array();
+ntext.forEach(d=>{
+    let e=[];
+    let date=d.slice(0,10);
+    let time=d.slice(11,16);
+    let message=d.slice(20,d.length);
+    message=message.split(':');
+    let sender=message[0];
+    if(message.length<2){
+        sender="";
+    }
+    message=message.join();
+    message=message.slice(sender.length+1, message.length);
+    sender?sender=sender:sender="whatsapp";
+    arr.push({date,time,sender,message});
+})
+return JSON.stringify(arr);
+}
