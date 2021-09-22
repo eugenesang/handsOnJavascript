2599f50730bd3663243a8e7cb47720a013137b27
Eugene Kiprop Sang <85967099+eugenesang@users.noreply.github.com>
2021-09-22 13:38:12 +0300

    Add files via upload

---
+++ word_puzle/wordMatch.js
@@ -0,0 +1,73 @@
+const {readFile}=require('fs');
+let combination=(str)=>{
+    var fn=function(active, rest, a){
+        if(!active && !rest) return;
+        if(!rest){
+            a.push(active);
+        } else{
+            fn(active+rest[0], rest.slice(1), a);
+            fn(active, rest.slice(1), a);
+        }
+        return a;
+    }
+    return fn("", str, []);
+}
+let permute=(nums)=>{
+    let result=[];
+    if(nums.length ===0) return [];
+    if(nums.length ===1) return [nums];
+    for(let i=0; i<nums.length; i++){
+        const currentNum=nums[i];
+        const remainingNums=nums.slice(0, i).concat(nums.slice(i+1));
+        const remainingNumsPermuted=permute(remainingNums);
+        for(let j=0; j<remainingNumsPermuted.length; j++){
+            const permutedArray=[currentNum].concat(remainingNumsPermuted[j]);
+            result.push(permutedArray);
+        }
+    }
+    return result;
+}
+function uniq(result){
+    let final=[];
+    result.forEach(d=>{
+        typeof(d)=='object'? final.push(d.join('')): final.push(d);
+        
+    })
+    result=new Set(final);
+    return result;
+}
+
+function makesSense(word, dictionary){
+    word=word.toLowerCase();
+    for(let w of dictionary){
+        w=w.toLowerCase();
+        if(word===w){
+            return true;
+        }
+    }
+    return false;
+};
+function allCombo(w, dict){
+   let words=uniq(permute(w));
+   let ww=[];
+   for(let word of words){
+       let d=combination(word);
+       d.forEach(dat=>ww.push(dat));
+   }
+   words=uniq(ww);
+    let ret=[];
+    for(let word of words){
+        if(makesSense(word, dict)){
+            ret.push(word);
+        }
+    }
+    return ret;
+};
+//nodejs only, you may just fetch the dictionary data from elsewhere
+readFile('words.json', (err,data)=>{
+    if(err) return err;
+    data=data.toString();
+    data=JSON.parse(data);
+    let ret=allCombo("leegnd", data);
+    console.log(ret);
+})
